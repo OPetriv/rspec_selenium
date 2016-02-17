09d9939c77b6d49d9f4ebdade5ace86e747524c5
OPetriv <oleh.petriv91@gmail.com>
2016-02-18 00:40:23 +0200

    new elements

--- spec/pages/checkboxes_page.rb
+++ spec/pages/checkboxes_page.rb
@@ -1,3 +1,8 @@
 class Checkboxes < SitePrism::Page
-	set_url '/'
+	set_url '/checkboxes'
+
+  element :title,   :xpath,"//div/h3"
+
+  element :checkbox_1,  :xpath,'//input[1]'
+  element :checkbox_2,  :xpath,'//input[2]'
 end
