8432fb95eba9214cf75f58250a996c29f59375f6
OPetriv <oleh.petriv91@gmail.com>
2016-02-17 21:39:31 +0200

    refactor

--- spec/elements/brokenimages_spec.rb
+++ spec/elements/brokenimages_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe 'Broken images'  do
 
   it 'Broken images' do
-    binding.pry
+
     @page = ElementsPages::Navigation.new
 
     visit ENV['SERVER']
 
-    expect(@page.elements_page.main.brokeimage[:href]).to eq("#{ENV['SERVER']}/broken_images")
-    expect(@page.elements_page.main.brokeimage.text).to eq("Broken Images")
+    expect(@page.main_page.main_section.brokeimage[:href]).to eq("#{ENV['SERVER']}/broken_images")
+    expect(@page.main_page.main_section.brokeimage.text).to eq("Broken Images")
 
-    @page.elements_page.main.brokeimage.click
+    @page.main_page.main_section.brokeimage.click
 
     expect(@page.brokenimages_page.title.text).to eq("Broken Images")
     
