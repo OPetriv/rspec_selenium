55046d9a8074b1c3be59f2bafcb2cbb6fb476fe4
OPetriv <oleh.petriv91@gmail.com>
2016-02-18 00:18:56 +0200

    finish change dom

--- spec/elements/challengingdom_spec.rb
+++ spec/elements/challengingdom_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe 'Challenging DOM'  do
 
 	it 'Challenging DOM' do
-	    @app = ElementsPages::Navigation.new
-		@app.main_page.load
+	  @page = ElementsPages::Navigation.new
+		@page.main_page.load
 
-		expect(@app.main_page.main_section.dom[:href]).to eq("#{mainurl}/challenging_dom")
-		expect(@app.main_page.main_section.dom.text).to   eq('Challenging DOM')
+		expect(@page.main_page.main_section.dom[:href]).to eq("#{ENV['SERVER']}/challenging_dom")
+		expect(@page.main_page.main_section.dom.text).to   eq('Challenging DOM')
 
 		@page.main_page.main_section.dom.click
 		expect(@page.challengingdom_page).to be_displayed
@@ -25,20 +25,20 @@ describe 'Challenging DOM'  do
 		@page.challengingdom_page.table_rows.each_with_index do |table_row, index|
 		  row_columns = table_row.all('td')
 		  expect(row_columns.count).to eq(7)
-		  expect(row_columns[0]).to    eq("Iuvaret#{index}")
-		  expect(row_columns[1]).to    eq("Apeirian#{index}")
-		  expect(row_columns[2]).to    eq("Adipisci#{index}")
-		  expect(row_columns[3]).to    eq("Definiebas#{index}")
-		  expect(row_columns[4]).to    eq("Consequuntur#{index}")
-		  expect(row_columns[5]).to    eq("Phaedrum#{index}")
-		  expect(row_columns[6]).to    eq('edit delete')
+		  expect(row_columns[0].text).to    eq("Iuvaret#{index}")
+		  expect(row_columns[1].text).to    eq("Apeirian#{index}")
+		  expect(row_columns[2].text).to    eq("Adipisci#{index}")
+		  expect(row_columns[3].text).to    eq("Definiebas#{index}")
+		  expect(row_columns[4].text).to    eq("Consequuntur#{index}")
+		  expect(row_columns[5].text).to    eq("Phaedrum#{index}")
+		  expect(row_columns[6].text).to    eq('edit delete')
 		end
 
 		@page.challengingdom_page.table_rows[3].find('a', text: 'edit').click
-		expect(current_url).to eq("#{mainurl}/challenging_dom#edit")
+		expect(current_url).to eq("#{ENV['SERVER']}/challenging_dom#edit")
 
 		@page.challengingdom_page.table_rows[3].find('a', text: 'delete').click
-		expect(current_url).to eq("#{mainurl}/challenging_dom#delete")
+		expect(current_url).to eq("#{ENV['SERVER']}/challenging_dom#delete")
 
 		expect(@page.challengingdom_page).to have_canvas
 	end
--- spec/pages/challengingdom_page.rb
+++ spec/pages/challengingdom_page.rb
@@ -7,10 +7,10 @@ class ChallengingDOM < SitePrism::Page
   element  :button_alert,   :xpath, "//*[contains(@class,\"button alert\")]"
   element  :button_success, :xpath, "//*[contains(@class,\"button success\")]"
 
-  element  :table,      '#content table'
-  element  :table_head, '#content table thead'
-  element  :table_body, '#content table tbody'
-  elements :table_rows, '#content table tbody tr'
+  element  :table,      :xpath,'//table'
+  element  :table_head, :xpath,'//thead'
+  element  :table_body, :xpath,'//tbody'
+  elements :table_rows, :xpath,'//tbody/tr'
 
   element :canvas, :xpath, "//*[@id=\"canvas\"]"
 end
