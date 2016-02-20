ade8592a10297abaa4f95e16a009e83530e81e4b
OPetriv <oleh.petriv91@gmail.com>
2016-02-21 00:42:49 +0200

    new elements

--- spec/pages/contextmenu_page.rb
+++ spec/pages/contextmenu_page.rb
@@ -1,3 +1,8 @@
 class ContextMenu < SitePrism::Page
-	set_url '/'
+	set_url '/context_menu'
+
+  element :title,   :xpath, "//div/h3"
+
+  element :hotspot, :xpath, "//*[@id=\"hot-spot\"]"
+    
 end
