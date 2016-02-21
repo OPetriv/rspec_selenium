bd1abbad4c22846d13e2f5f79a01f8c3fa583767
OPetriv <oleh.petriv91@gmail.com>
2016-02-21 12:00:37 +0200

    Disappearing elements spec

---
+++ spec/elements/disappearingelements_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'Disappearing elements'  do
+
+  it 'Disappearing elements' do
+    @page = ElementsPages::Navigation.new
+    @page.main_page.load
+
+    expect(@page.main_page.main_section.disselement[:href]).to eq("#{ENV['SERVER']}/disappearing_elements")
+    expect(@page.main_page.main_section.disselement.text).to eq("Disappearing Elements")
+
+    @page.main_page.main_section.disselement.click
+binding.pry
+    expect(@page.disappearingelements_page.title.text).to eq('Disappearing Elements')
+
+  end
+end
