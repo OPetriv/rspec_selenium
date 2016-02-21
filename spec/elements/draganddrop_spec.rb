dfc271d3f5be5608be12d336e55de3a3b2537cfe
OPetriv <oleh.petriv91@gmail.com>
2016-02-21 16:23:41 +0200

    finish dnd spec

--- spec/elements/draganddrop_spec.rb
+++ spec/elements/draganddrop_spec.rb
@@ -13,5 +13,19 @@ describe 'Drag and drop'  do
 
     expect(@page.draganddrop_page.title.text).to eq('Drag and Drop')
 
+    expect(@page.draganddrop_page.column_a.text).to eq('A')
+    expect(@page.draganddrop_page.column_b.text).to eq('B')
+
+    dnd_javascript = File.read(Dir.pwd + '/spec/support/dnd.js')
+    page.driver.browser.execute_script(dnd_javascript+"$('#column-a').simulateDragDrop({ dropTarget: '#column-b'});")
+
+    expect(@page.draganddrop_page.column_a.text).to eq('B')
+    expect(@page.draganddrop_page.column_b.text).to eq('A')
+
+    page.driver.browser.execute_script(dnd_javascript+"$('#column-a').simulateDragDrop({ dropTarget: '#column-b'});")
+
+    expect(@page.draganddrop_page.column_a.text).to eq('A')
+    expect(@page.draganddrop_page.column_b.text).to eq('B')
   end
 end
+
