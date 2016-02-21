ddfb62968d24c5964014044c8979f38435967837
OPetriv <oleh.petriv91@gmail.com>
2016-02-21 16:23:27 +0200

    new element

--- spec/pages/draganddrop_page.rb
+++ spec/pages/draganddrop_page.rb
@@ -1,5 +1,8 @@
 class DragandDrop < SitePrism::Page
 	set_url '/drag_and_drop'
 
-  element :title, :xpath,"//div/h3"
+  element :title,     :xpath,"//div/h3"
+
+  element :column_a,  '#column-a'  
+  element :column_b,  '#column-b'  
 end
