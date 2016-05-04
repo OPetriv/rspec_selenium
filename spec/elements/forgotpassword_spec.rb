b3f58f3b5408ff63fc102750b81ca213922cb45d
OPetriv <oleh.petriv91@gmail.com>
2016-05-04 19:10:57 +0300

    parse email

--- spec/elements/forgotpassword_spec.rb
+++ spec/elements/forgotpassword_spec.rb
@@ -9,5 +9,22 @@ describe 'Forgot password'  do
     expect(@page.main_page.main_section.forgotpass[:href]).to eq("#{ENV['SERVER']}/forgot_password")
 		expect(@page.main_page.main_section.forgotpass.text).to eq("Forgot Password")
 		@page.main_page.main_section.forgotpass.click
+
+		expect(@page.forgotpassword_page.title.text).to eq('Forgot Password')
+		@page.forgotpassword_page.email.set 'minimovesautotest@gmail.com'
+		@page.forgotpassword_page.retrievebut.click
+		@page.forgotpassword_page.wait_for_content
+		expect(@page.forgotpassword_page.content.text).to eq('Your e-mail\'s been sent!')
+binding.pry
+		gmail = Gmail.connect('minimovesautotest@gmail.com', 'm*i&n^i%m$o#v@e!sautotest')
+		@email = gmail.inbox.emails(:unread, from: 'no-reply@the-internet.herokuapp.com').last
+
+		message_body = @email.message.body.raw_source
+		url =  message_body.scan(/https?:\/\/[\S]+/).last
+  	username = message_body.scan(/username: (.*)$/)[0][0].strip
+  	password = message_body.scan(/password: (.*)$/)[0][0].strip
+
+  	visit url
+
 end
 end
--- spec/pages/forgotpassword_page.rb
+++ spec/pages/forgotpassword_page.rb
@@ -1,5 +1,9 @@
 class ForgotPassword < SitePrism::Page
 	set_url '/forgot_password'
 
-	element	:title, :xpath,"//div/h3"
+	element	:title, 				:xpath,'//*[@id="content"]/div/h2'
+
+	element :email,					:xpath,'//*[@id="email"]'
+	element :retrievebut, 	:xpath,'//*[@id="form_submit"]/i'
+	element :content,				:xpath,'//*[@id="content"]'
 end
